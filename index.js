a5a51710b9f91fda4321fb0a023ea33834f69152
sophiasagan <sophiabraddock82@gmail.com>
2019-11-01 20:48:43 -0400

    stretch

--- index.js
+++ index.js
@@ -160,8 +160,15 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  let unique = [];
+  for (element of list) {
+    let isInList = unique.includes(element);
+    if (!isInList) {
+      unique.push(element);
+    }
+  }
+  return callback(unique);
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
